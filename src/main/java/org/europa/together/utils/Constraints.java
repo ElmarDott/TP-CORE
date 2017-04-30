ca67ecded2de08b63bd72268498f7a3e5344d2eb
john doe <john@doe.org>
2017-04-30 07:39:59 +0200

    [KM-0000] #INIT 'archetype:jar' {Create Project structure for the CORE Library.}

---
+++ src/main/java/org/europa/together/utils/Constraints.java
@@ -0,0 +1,29 @@
+package org.europa.together.utils;
+
+/**
+ * Constraints for the package CORE.
+ */
+public final class Constraints {
+
+    /**
+     * Constructor.
+     */
+    private Constraints() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Module name.
+     */
+    public static final String MODULE_NAME = "core";
+
+    /**
+     * Version of the module.
+     */
+    public static final String MODULE_VERSION = "1.0";
+
+    /**
+     * A short description of the module.
+     */
+    public static final String MODULE_DESCRIPTION = "Basic Library";
+}
