d685d807f0f69d7a87b787b5169f8d44fe57593f
ElmarDott <elmar.dott@gmail.om>
2018-05-25 18:59:17 -0500

    [CORE-0001] #IMPLEMENT 'function:LoggingService' {Add test scenarios for LoggingService)

---
+++ src/main/java/org/europa/together/service/LoggingService.java
@@ -0,0 +1,103 @@
+package org.europa.together.service;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import org.apiguardian.api.API;
+import static org.apiguardian.api.API.Status.STABLE;
+import org.europa.together.application.LoggerImpl;
+import org.europa.together.application.XmlToolsImpl;
+import org.europa.together.business.Logger;
+import org.europa.together.business.XmlTools;
+import org.europa.together.domain.LogLevel;
+import org.europa.together.utils.Constraints;
+import org.europa.together.utils.FileUtils;
+import org.springframework.stereotype.Service;
+
+/**
+ * Implementation of the Logging Service.
+ *
+ * @author elmar.dott@gmail.com
+ * @version 1.0
+ * @since 1.0
+ */
+@API(status = STABLE, since = "1.1")
+@Service
+public final class LoggingService {
+
+    private static final Logger LOGGER = new LoggerImpl(LoggingService.class);
+
+    /**
+     * Constructor.
+     */
+    @API(status = STABLE, since = "1.1")
+    public LoggingService() {
+        LOGGER.log("instance class", LogLevel.INFO);
+    }
+
+    /**
+     * Creates for an application a external log configuration. Copy the
+     * configuration file for the Logger from the classpath to the application
+     * directory.
+     */
+    @API(status = STABLE, since = "1.1")
+    public void createLogConfiguration() {
+        try {
+            String destination = Constraints.SYSTEM_APP_DIR + "/logback.xml";
+            Files.copy(getClass().getClassLoader().getResourceAsStream("logback.xml"),
+                    Paths.get(destination), StandardCopyOption.REPLACE_EXISTING);
+
+            LOGGER.log("Copy File to: " + destination, LogLevel.DEBUG);
+        } catch (Exception ex) {
+            LOGGER.catchException(ex);
+        }
+    }
+
+    /**
+     * Read the log configuration from a file and return the file content.
+     *
+     * @param file as String
+     * @return configuration as String
+     */
+    @API(status = STABLE, since = "1.1")
+    public String readLogConfiguration(final String file) {
+        String configuration = null;
+        try {
+            configuration = FileUtils.readFileStream(new File(file));
+        } catch (Exception ex) {
+            LOGGER.catchException(ex);
+        }
+        return configuration;
+    }
+
+    /**
+     * Write a configuration to a file.
+     *
+     * @param content as String
+     * @param file as String
+     */
+    @API(status = STABLE, since = "1.1")
+    public void writeLogConfiguration(final String content, final String file) {
+
+        try {
+
+            XmlTools xmlTools = new XmlToolsImpl();
+            xmlTools.writeXmlToFile(content, file);
+            LOGGER.log("try to update logger configuration to: " + file, LogLevel.DEBUG);
+
+            File verify = new File(file);
+            if (verify.exists()) {
+                xmlTools.parseXmlFile(verify);
+                if (!xmlTools.isWellFormed()) {
+                    LOGGER.log("xml is not wellformed, file can not updated.", LogLevel.WARN);
+                    verify.delete();
+                } else {
+                    xmlTools.writeXmlToFile(xmlTools.prettyPrintXml(), file);
+                }
+            }
+        } catch (Exception ex) {
+            LOGGER.catchException(ex);
+        }
+    }
+}
---
+++ src/test/java/org/europa/together/service/LoggingServiceOutcome.java
@@ -0,0 +1,67 @@
+package org.europa.together.service;
+
+import com.tngtech.jgiven.Stage;
+import java.io.File;
+import org.europa.together.application.LoggerImpl;
+import org.europa.together.business.Logger;
+import org.europa.together.utils.Constraints;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * JGiven PostConditions for LoggingService Test Scenarios.
+ */
+@SuppressWarnings("unchecked")
+@RunWith(JUnitPlatform.class)
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath:org/europa/together/configuration/spring-dao-test.xml"})
+public class LoggingServiceOutcome extends Stage<LoggingServiceOutcome> {
+
+    private static final Logger LOGGER
+            = new LoggerImpl(LoggingServiceOutcome.class);
+
+    private String configFile = Constraints.SYSTEM_APP_DIR + "/logback.xml";
+    private final LoggingService loggingService = new LoggingService();
+
+    public LoggingServiceOutcome configuration_is_present_in_application_directory() {
+
+        try {
+            File config = new File(configFile);
+            assertTrue(config.exists());
+        } catch (Exception ex) {
+            LOGGER.catchException(ex);
+        }
+        return self();
+    }
+
+    public LoggingServiceOutcome configuration_is_readed() {
+
+        try {
+            String config = loggingService.readLogConfiguration(configFile);
+            assertTrue(config.contains("Logback Configuration Template"));
+        } catch (Exception ex) {
+            LOGGER.catchException(ex);
+        }
+        return self();
+    }
+
+    public LoggingServiceOutcome configuration_is_updated() {
+
+        try {
+            String oldConfig = loggingService.readLogConfiguration(configFile);
+            String newConfig = loggingService.readLogConfiguration(Constraints.SYSTEM_APP_DIR + "/target/logback.xml");
+
+            assertTrue(oldConfig.contains("Logback Configuration Template"));
+            assertTrue(newConfig.contains("com.tngtech.jgiven"));
+            assertNotEquals(newConfig, oldConfig);
+
+        } catch (Exception ex) {
+            LOGGER.catchException(ex);
+        }
+        return self();
+    }
+}
