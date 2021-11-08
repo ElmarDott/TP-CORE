81ae36257822232aa64be892142821cb5110e0f4
Elmar Dott <elmar.dott@gmail.com>
2021-11-08 15:01:31 -0600

    [CORE-0011] #IMPLEMENT 'function:OpenPdfRenderer' {Wrap PDFReader Class to decuple the library from API}

--- src/main/java/org/europa/together/application/internal/PdfDocument.java
+++ src/main/java/org/europa/together/application/internal/PdfDocument.java
@@ -4,16 +4,16 @@ import com.lowagie.text.pdf.PdfReader;
 import java.io.IOException;
 
 /**
- * This class hide implementation details of the OpenPdf PdfRendere class used
+ * This class hides implementation details of the OpenPdf PdfRendere class used
  * in the API.
  */
 public class PdfDocument extends PdfReader {
 
-    public PdfDocument(String filename) throws IOException {
+    public PdfDocument(final String filename) throws IOException {
         super(filename);
     }
 
-    public PdfDocument(PdfReader reader) throws IOException {
+    public PdfDocument(final PdfReader reader) throws IOException {
         super(reader);
     }
 }
